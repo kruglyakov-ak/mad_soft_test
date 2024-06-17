17e6a7658f3a4a8fc0a5649c86a66fd3be311c1a
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2024-06-18 00:22:24 +0300

    ci: deploy to gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/mad_soft_test",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
